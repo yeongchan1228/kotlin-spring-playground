2188a1faf7294f69cb771de7061a6301367bd9d4
YC <gks54410@gmail.com>
2023-09-24 14:33:19 +0900

    initial commit

---
+++ src/main/kotlin/study/kotlinspringplayground/KotlinSpringPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package study.kotlinspringplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/study/kotlinspringplayground/KotlinSpringPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.kotlinspringplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
